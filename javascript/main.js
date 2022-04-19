3ef69bfab3594aa9707bd0400dd28c898f43b2e2
AGLcaicai <admin@heavensec.org>
2022-04-19 16:47:35 +0800

    add countdown

--- javascript/main.js
+++ javascript/main.js
@@ -22,3 +22,27 @@ window.onscroll = function () {
   }
 };
 //# sourceMappingURL=main.js.map
+
+
+// flipdowm
+document.addEventListener('DOMContentLoaded', () => {
+
+  // Unix timestamp (in seconds) to count down to
+  // var twoDaysFromNow = (new Date().getTime() / 1000) + (86400 * 2) + 1;
+
+  // Set up FlipDown
+  var flipdown = new FlipDown(1656734400, {theme: "light"})
+    
+    // Start the countdown
+    .start()
+
+    // Do something when the countdown ends
+    .ifEnded(() => {
+      console.log('The countdown has ended!');
+    });
+
+  // Toggle theme
+
+  var ver = document.getElementById('ver');
+  ver.innerHTML = flipdown.version;
+});
